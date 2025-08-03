408ae694a673395fe74504dd3c41362c8664ca17
ahmniab <ahmniab11@gmail.com>
2025-08-04 02:33:37 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { globalTheme } from './theme';
+import Checkout from './Pages/Checkout'; 
+import Confirmation from './Pages/Confirmation';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/checkout/:roomId" element={<Checkout/>} />
+          <Route path="/confirmation" element={<Confirmation/>} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </Router>

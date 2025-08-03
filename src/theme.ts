e50c532134b7d9e6e26621226aa9d2bebbf7d90f
ahmniab <ahmniab11@gmail.com>
2025-08-03 13:33:58 +0300

    Added Global Theme

---
+++ src/theme.ts
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles';
+
+export const globalTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+    background: {
+      default: '#121212',
+      paper: '#1e1e1e',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  }
+});
